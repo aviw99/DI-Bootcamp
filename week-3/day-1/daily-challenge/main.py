d8fb22e727154a7fbe244cf95efd9b507cc1101a
aviw99 <avrahammw@gmail.com>
2023-04-02 16:38:37 +0300

    dc

--- week-3/day-1/daily-challenge/main.py
+++ week-3/day-1/daily-challenge/main.py
@@ -15,9 +15,23 @@ class Farm():
             print(f"{key}: {value}")
         print('E-I-E-I-O!')
         
+    def get_animal_types(self):
+        new_list = []
+        for key, value in self.animals.items():
+            new_list += [key]
+        return new_list
+    
+    def get_short_info(self):
+        newer_list = []
+        for i in self.get_animal_types():
+            newer_list += [i]
+        print(f"McDonald's farm has {newer_list[0]}s, {newer_list[2]}s and {newer_list[1]}.")
+
 macdonald = Farm("McDonald")
 macdonald.add_animal('cow',5)
 macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
 macdonald.farm_info()
+print(macdonald.get_animal_types())
+macdonald.get_short_info()
