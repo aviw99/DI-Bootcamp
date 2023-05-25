8d1a9bef39bae849b6acf52a5b72fb3ca208641f
aviw99 <avrahammw@gmail.com>
2023-05-25 23:49:37 +0300

    game

--- week-8/day-5/tictactoe2/script.js
+++ week-8/day-5/tictactoe2/script.js
@@ -9,12 +9,14 @@ const choose = document.getElementById('choose');
 
 choose.remove();
 textOne.innerText = 'Tic-Tac-Toe';
-textDivMain.style.color = '#c2730a';
+textDivMain.style.color = '#ffb2ab';
 textDivMain.style.borderRadius = '10px';
 textDivMain.style.width = '304px';
 textDivMain.style.height = '440px';
 textDivMain.style.backgroundColor = 'rgba(249,194,151,0.48)';
 textDivMain.style.textAlign = 'center';
+textDivMain.style.borderRight = 'black solid 2px';
+textDivMain.style.borderBottom = 'black solid 2px';
 textDivMain.appendChild(textOne);
 textDivMain.appendChild(choose);
 textDivMain.style.position = 'absolute';
@@ -27,49 +29,85 @@ const rows = [];
 
 function gameRender(user){
     if(user === 'X'){
-        userX()
+        console.log('2-user is x')
+        userX(user)
     }else if(user === 'O'){
-        console.log('user is o')
+        console.log('2-user is o')
+        userO(user)
     }
 }
-function userX(){
-    rows.forEach((rowDiv)=>{
-        rowDiv.forEach((cell)=>{
-            cell.addEventListener('click', function(){
-                cell.style.backgroundColor = 'red';
-            })
-        })
-    })
+function userX(user){
+    console.log('3-running userX function');
+    gameBoard(user)
+}
+function userO(user){
+    console.log('3-running userO function');
+    gameBoard(user)
 }
 
 
+let colors = ["#c20a57", "#c2730a"];
+let currentIndex = 0;
+wrap.addEventListener('dblclick', function() {
+    let currentColor = colors[currentIndex];
+    console.log(currentColor);
+    wrap.style.backgroundColor = currentColor;
+    currentIndex = (currentIndex + 1) % colors.length;
+});
 // creating game board
-for(let j = 0; j < 3; j++){
-    const rowDiv = document.createElement('div');
-    for (let i = 0; i < 3; i++) {
-        const cell = document.createElement('div');
-        cell.style.borderRadius = '10px';
-        cell.style.width = '88px';
-        cell.style.height = '88px';
-        cell.style.margin = '5px';
-        cell.style.marginTop = '10px';
-        cell.style.backgroundColor = '#ffd7af';
-        cell.classList.add('cell');
-
-        rowDiv.appendChild(cell)
+function gameBoard(user){
+    if(!user) {
+        console.log('1-user not defined')
+        for (let j = 0; j < 3; j++) {
+            const rowDiv = document.createElement('div');
+            for (let i = 0; i < 3; i++) {
+                const cell = document.createElement('div');
+                cell.style.borderRadius = '10px';
+                cell.style.width = '88px';
+                cell.style.height = '88px';
+                cell.style.margin = '5px';
+                cell.style.marginTop = '10px';
+                cell.style.backgroundColor = '#ffb2ab';
+                cell.classList.add('cell');
+                rowDiv.appendChild(cell);
+            }
+            rows.push(rowDiv)
+        }
+        return rows
+    }else if(user){
+        console.log(`4-${user}`)
+        for (let j = 0; j < 3; j++) {
+            const rowDiv = document.createElement('div');
+            for (let i = 0; i < 3; i++) {
+                const cellU = document.createElement('div');
+                cellU.style.borderRadius = '10px';
+                cellU.style.width = '88px';
+                cellU.style.height = '88px';
+                cellU.style.margin = '5px';
+                cellU.style.marginTop = '10px';
+                cellU.style.backgroundColor = '#ffb2ab';
+                cellU.classList.add('cell');
+                cellU.addEventListener('click', clicked);
+                rowDiv.appendChild(cellU);
+            }
+            rows.push(rowDiv)
+        }
+        return rows
     }
-    rows.push(rowDiv)
 }
-
+function clicked(e){
+    const value = e.target.value;
+    console.log(value);
+}
 
 // render game function
 function render(arr){
+    arr = gameBoard()
     arr.forEach((rowDiv) => {
+        wrap.appendChild(rowDiv);
         wrap.appendChild(textDivMain);
-        wrap.appendChild(rowDiv)
     })
 }
-
 
 // render new game on load
 window.onload = ()=>{
@@ -81,13 +119,11 @@ window.onload = ()=>{
         const user = 'X';
         const computer = 'O';
         gameRender(user, computer);
-        // render(new rows setup)
     });
     oButton.addEventListener('click', function() {
         // set user to o and computer to x
         const user = 'O';
         const computer = 'X';
         gameRender(user, computer);
-        // render(new rows setup)
     });
 }
