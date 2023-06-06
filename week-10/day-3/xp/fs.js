eed8db9eed1f0e2fcde1fb24fbacaa2396777ac6
aviw99 <avrahammw@gmail.com>
2023-06-06 14:31:25 +0300

    xp

---
+++ week-10/day-3/xp/fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+
+// fs.readFile('text.txt', 'utf-8', (err,text)=>{
+//     if(err) {
+//         console.log(err)
+//     }else{
+//         console.log(text)
+//     }
+// })
+// const str = 'some text';
+// fs.writeFile('data.txt', str, (err)=>{
+//     console.log(err)
+// })
+//
+// fs.appendFile('data.txt', '\n buy orange juice','utf-8',(err)=>{
+//     console.log(err)
+// } )
+
+// fs.unlink('data.txt',(err)=>{})
---
+++ week-10/day-3/xp/script.js
@@ -0,0 +1,8 @@
+const fs = require('fs').promises;
+
+const str = 'Some Text To See';
+
+// fs.writeFile('text.txt', str, (err)=>{
+//     console.log(err)
+// })
+
