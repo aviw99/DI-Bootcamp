79953a8970d99c7ec3cc30f25f4268f4a2c0a3a8
aviw99 <avrahammw@gmail.com>
2023-06-11 15:21:18 +0300

    dc

---
+++ week-11/day-1/daily-challenge/dc-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyCarousel from './Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyCarousel/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ week-11/day-1/daily-challenge/dc-app/src/Carousel.js
@@ -0,0 +1,29 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import './Carousel.css'
+const MyCarousel = () => {
+    return (
+        <Carousel className="card-styles"  infiniteLoop autoPlay>
+            <div>
+                <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg"/>
+                <p className="legend">Hong Kong</p>
+            </div>
+            <div>
+                <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"/>
+                <p className="legend">Macao</p>
+            </div>
+            <div>
+                <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"/>
+                <p className="legend">Japan</p>
+            </div>
+            <div>
+                <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"/>
+                <p className="legend">Las Vegas</p>
+            </div>
+        </Carousel>
+    )
+        
+    
+}
+
+export default MyCarousel
